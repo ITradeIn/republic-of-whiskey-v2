df5190ce9e5e7055a7749299b4070929cd59439b
Albert Miclat <albert@istackmanila.com>
2016-12-14 03:51:43 +0800

    Website updates

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -7,6 +7,11 @@
     setTimeout(function() {
       $('.curve').circleType({radius: 1800});
     }, 1000);
+    $('#submit-newletter-form').click(function () {
+      $('#mc-embedded-subscribe-form').submit();
+    });
+    $('body').scrollspy({ target: '#scroll-nav' });
+    $('#our-whiskey-1').stellar();
   });
 
 })(jQuery, window, document);
